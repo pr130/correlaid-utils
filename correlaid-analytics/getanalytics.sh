f9b799266f35d8165044f70ddfec094890f73829
Frie's Raspberry <friedrike.preu@gmail.com>
2017-06-05 11:08:45 +0000

    remove weekly files

--- correlaid-analytics/getanalytics.sh
+++ correlaid-analytics/getanalytics.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 Rscript /home/fripi/correlaid/correlaid-utils/correlaid-analytics/get_analytics.R
+Rscript /home/fripi/correlaid/correlaid-utils/correlaid-analytics/get_network_stats.R
