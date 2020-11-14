25f862bc7980436f7fca0ff62ac3dafd6bc4747a
Frie <fr1e@pm.me>
2020-11-14 20:39:23 +0100

    use here to make paths error-proof

--- correlaid-analytics/02_git.R
+++ correlaid-analytics/02_git.R
@@ -1,4 +1,5 @@
 # git2r
-git2r::add(path = "correlaid-analytics/data/")
+git2r::pull()
+git2r::add(path = here::here("correlaid-analytics/data/"))
 git2r::commit(message = "update daily data")
 git2r::push()
--- correlaid-analytics/run.R
+++ correlaid-analytics/run.R
@@ -1,3 +1,4 @@
-source("correlaid-analytics/01_get_daily_analytics.R")
-source("correlaid-analytics/02_git.R")
+library(here)
+source(here::here("correlaid-analytics/01_get_daily_analytics.R"))
+source(here::here("correlaid-analytics/02_git.R"))
 
