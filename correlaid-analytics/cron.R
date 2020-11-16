b8bb2508435d5b1a48bda8fce72832f671421ccf
Frie <fr1e@pm.me>
2020-11-16 10:16:35 +0100

    fix cronjob with workdir

--- correlaid-analytics/cron.R
+++ correlaid-analytics/cron.R
@@ -1,8 +1,7 @@
 library(cronR)
 # RUN ONCE TO CONFIGURE CRONJOB
-f <- normalizePath("run.R")
-f
-cmd <- cron_rscript(f)
+f <- normalizePath(here::here("correlaid-analytics", "run.R"))
+cmd <- cron_rscript(f, workdir = "/home/frie/correlaid-utils")
 cmd
 
 cron_add(command = cmd, frequency = 'daily', at = "23:50", id = 'daily_analytics', description = 'Get daily CorrelAid Analytics')
