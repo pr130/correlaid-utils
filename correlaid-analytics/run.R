26097720a3dbc8eaf525e65df72258f2ec6c203b
Frie <fr1e@pm.me>
2020-11-18 22:25:57 +0100

    add log

--- correlaid-analytics/run.R
+++ correlaid-analytics/run.R
@@ -1,4 +1,6 @@
 library(here)
+print("==============================")
+print(Sys.time())
 source(here::here("correlaid-analytics/01_get_daily_analytics.R"))
 source(here::here("correlaid-analytics/02_git.R"))
 
